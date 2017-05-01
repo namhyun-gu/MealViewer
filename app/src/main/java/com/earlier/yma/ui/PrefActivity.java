311cad376b08ae67795c6caef2156c242ec72c1a
namhyun-gu <mnhan0403@gmail.com>
2017-05-01 23:30:55 +0900

    Remove butterknife

--- app/src/main/java/com/earlier/yma/ui/PrefActivity.java
+++ app/src/main/java/com/earlier/yma/ui/PrefActivity.java
@@ -17,20 +17,15 @@
 package com.earlier.yma.ui;
 
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 import com.earlier.yma.R;
 import com.earlier.yma.ui.fragment.InfoFragment;
 import com.earlier.yma.ui.fragment.SettingsFragment;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
 
 public class PrefActivity extends AppCompatActivity {
     public static String BUNDLE_TYPE = "preference_type";
@@ -39,25 +34,14 @@ public class PrefActivity extends AppCompatActivity {
     public static int TYPE_SETTINGS = 0x00001;
     public static int TYPE_INFORMATION = 0x0002;
 
-    @BindView(R.id.toolbar) Toolbar mToolbar;
-    @BindView(R.id.shadow_view) View mShadowView;
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_with_preference);
 
-        // Bind Butterknife
-        ButterKnife.bind(this);
-
         // Initial Toolbar
-        setSupportActionBar(mToolbar);
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        // Toolbar Shadow (Pre lollipop)
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
-            mShadowView.setVisibility(View.VISIBLE);
-        }
 
         if (savedInstanceState == null) {
             PreferenceFragment fragment = null;
